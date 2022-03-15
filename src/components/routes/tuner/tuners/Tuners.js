c93505076625fda1332c0e5d13355776fa2c726c
brianross93 <brssnj45@gmail.com>
2022-03-15 16:27:36 -0400

    adding tuner iframe

--- src/components/routes/tuner/tuners/Tuners.js
+++ src/components/routes/tuner/tuners/Tuners.js
@@ -4,6 +4,7 @@ export default function Tuners() {
   return (
     <div className="Tuners">
       <h1>Tuners</h1>
+      <iframe src="http://websdr.ewi.utwente.nl:8901" width="100%" height="100%" />
     </div>
   );
 }
