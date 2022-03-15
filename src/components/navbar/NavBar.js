0cb93082a65d5ec783c226ab944b1e1a847c7289
djaskulak <danikaherminise@gmail.com>
2022-03-15 13:55:52 -0700

    added navbar styling

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -4,11 +4,85 @@ import './NavBar.css'
 export default function NavBar() {
   return (
     <div className="NavBar">
-      <ul>
-        <li><NavLink to='/'>Home</NavLink></li>
-        <li><NavLink to='/tuner'>Tuner</NavLink></li>
-        <li><NavLink to='/recordings'>Recordings</NavLink></li>
-        <li><NavLink to='/charities'>Charities</NavLink></li>
+      <ul className="nav">
+        <li>
+          <ul className="title">
+            <li><img id="logo" src="./logo_white.png" alt="logo"/></li>
+            <li id="title">War Chatter</li>
+          </ul>
+        </li>
+        <li>
+          <ul className="navlinks">
+            <li>
+              <NavLink 
+                to='/' 
+                className={'link'} 
+                style={({ isActive }) =>
+                  isActive
+                    ? {
+                        color: '#FFFFFF',
+                      }
+                    : { 
+                        color: '#B1B1B1', 
+                      }
+                    }
+              >
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/tuner' 
+                className={'link'}
+                style={({ isActive }) =>
+                isActive
+                  ? {
+                      color: '#FFFFFF',
+                    }
+                  : { 
+                      color: '#B1B1B1', 
+                    }
+                  }
+              >
+                Tuner
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/recordings' 
+                className={'link'}
+                style={({ isActive }) =>
+                isActive
+                  ? {
+                      color: '#FFFFFF',
+                    }
+                  : { 
+                      color: '#B1B1B1', 
+                    }
+                  }
+              >
+                Recordings
+              </NavLink>
+            </li>
+            <li>
+              <NavLink 
+                to='/charities' 
+                className={'link'}
+                style={({ isActive }) =>
+                isActive
+                  ? {
+                      color: '#FFFFFF',
+                    }
+                  : { 
+                      color: '#B1B1B1', 
+                    }
+                  }
+              >
+                Charities
+              </NavLink>
+            </li>
+          </ul>
+        </li>
       </ul>
     </div>
   );
