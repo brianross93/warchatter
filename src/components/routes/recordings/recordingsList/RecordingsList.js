38cab054f2a8ed40f683430ca51dff4c3a1b6399
brianross93 <brssnj45@gmail.com>
2022-03-17 06:02:04 -0400

    added error handling to recordings page and updated info

--- src/components/routes/recordings/recordingsList/RecordingsList.js
+++ src/components/routes/recordings/recordingsList/RecordingsList.js
@@ -1,18 +1,26 @@
+// Refactored the code to include error handling.
 import './RecordingsList.css'
 import recordings from "./recordings.json";
-
 export default function RecordingsList() {
-  return (
-    <div className="RecordingsList">
-      <h1>List</h1>
-      <ul>
-        {recordings.map((recording, i) => (
-          <li key={i}>
-            <a href={recording.link}>{recording.title}</a>
-            <p>{recording.time}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+  try {
+    return (
+      <div className="RecordingsList">
+        <h1>List</h1>
+        <ul>
+          {recordings.map(recordings => (
+            <li key={recordings.id}>
+              <a href={recordings.url}>{recordings.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="RecordingsList">
+        <h1>List</h1>
+        <p>There was an error loading the recordings list</p>
+      </div>
+    );
+  }
 }
--- src/components/routes/tuner/tunerBanner/TunerBanner.js
+++ src/components/routes/tuner/tunerBanner/TunerBanner.js
@@ -1,12 +1,15 @@
 import './TunerBanner.css'
-
+// Come back and create link to submissions area of page
 export default function TunerBanner () {
   return (
     <div className='TunerBanner'>
       <h1>Tuners</h1>
       <h3>Instructions: </h3>
       <ol>
-        <li>list instructions here</li>
+        <li>Click "Start Audio" to enable sound</li>
+        <li>Find the numbers (below the waterfall graph)</li>
+        <li>Below are frequencies that have been used by the Russian Military. You can check out the various frequencies and see if you hear anything</li>
+        <li>There is a record button on the site. If you record any Russian Military Chatter, be sure to submit it here</li>
       </ol>
     </div>
   )
