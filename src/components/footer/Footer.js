e9c17e7f47a46b5604ae105fbc77b9ef5bccf9ae
brianross93 <brssnj45@gmail.com>
2022-03-18 17:40:39 -0400

    adding discord again

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,8 +8,8 @@ export default function Footer() {
         <a href="https://twitter.com/CashmanDian"><img src="./twitter.png" alt="twitter logo"/></a>
       </div>
       <div className="discord">
-        <p>Join our Discord server: </p>
-        <a href="https://discord.com/invite/VuFv525M"><img src="./discord.png" alt="discord logo" /></a>
+        <p>Join the Discord server: </p>
+        <a href="https://discord.gg/zf4NCD2d"><img src="./discord.png" alt="discord logo" /></a>
       </div>
     </div>
   )
