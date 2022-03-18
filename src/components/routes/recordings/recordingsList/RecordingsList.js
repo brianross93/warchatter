c38f7b831b22983503f78426e02bcf7b80db5008
brianross93 <brssnj45@gmail.com>
2022-03-18 01:36:22 -0400

    adding translations

--- src/components/routes/recordings/recordingsList/RecordingsList.js
+++ src/components/routes/recordings/recordingsList/RecordingsList.js
@@ -8,6 +8,26 @@ export default function RecordingsList() {
 
   function getTranscript(){
     console.log(transcript4034);
+    // This function will display the transcript in a div below the list of recordings
+    // To do this, we need to create a div with a class of "transcript"
+    // Then we need to create a paragraph tag with the transcript text
+    // Then we need to append the paragraph tag to the div
+    // Then we need to append the div to the body
+    // Code:
+    const transcriptDiv = document.createElement('div');
+    const transcriptP = document.createElement('p');
+    transcriptP.innerHTML = transcript4034;
+    transcriptDiv.appendChild(transcriptP);
+    document.body.appendChild(transcriptDiv);
+
+    // also open the text in a small window off to the side
+    // Code:
+    const transcriptWindow = window.open('', 'transcriptWindow', 'width=500,height=500');
+    transcriptWindow.document.write(transcript4034);
+
+    
+
+
   }
   return (
     <>
@@ -33,6 +53,10 @@ export default function RecordingsList() {
       </ul>
       <button onClick={() => getTranscript()}>Transcribe</button>
     </div>
+    {/* Add a div for the translation to display */}
+    {/* It shoulkd display the translation when the Transcribe button is pressed */}
+
+    
     </>
   );
 }
