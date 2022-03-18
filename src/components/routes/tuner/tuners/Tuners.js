2d724ac4a8afc6eb80d2f3ed7e0f12ca850f4df5
brianross93 <brssnj45@gmail.com>
2022-03-18 16:45:50 -0400

    adding link to frequency tuner

--- src/components/routes/tuner/tuners/Tuners.js
+++ src/components/routes/tuner/tuners/Tuners.js
@@ -2,6 +2,19 @@ import './Tuners.css'
 
 export default function Tuners() {
   return (
+
+    <div className="Tuner-link">
+      {/* link to this site "http://websdr.ewi.utwente.nl:8901" */}
+      <a href="http://websdr.ewi.utwente.nl:8901">Twente Radio</a>
+      
+    
+
+    
+      <div className="Tuner-link-text">
+      
+
+      <h1>Tuners</h1>
+      </div>
     <div className="Tuners">
       <iframe 
         title="tuner"
@@ -10,5 +23,15 @@ export default function Tuners() {
         height="500px" 
       />
     </div>
+    </div>
   );
 }
+
+
+
+
+// const Tuners = () => {
+//   const tuners = [
+//       { link: "http://websdr.ewi.utwente.nl:8901", name: "University of Twente Radio Listener" },
+//       { link: "http://84.201.157.25:26057", name: "Alternate Radio Listener (UKR)" },
+//   ];
